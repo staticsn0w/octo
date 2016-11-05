97019a273d0b1d1e2c40ed9a86781bdf8db09dba
sad mac <staticsn0w@users.noreply.github.com>
2016-11-05 15:36:16 -0400

    Update octo.sh

--- octo.sh
+++ octo.sh
@@ -1,16 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 
 cd /usr/ports
 ls
-printf "This is /usr/ports, what would you like to install?"
+echo "this is everything in your /usr/ports directory, what would you like to install?"
 read input
-cd $input
-printf "Does this package require 'make install?'\n[y/n]"
+if [ -d "$input" ]; then
+  cd $input
+else
+  echo "ERROR! PACKAGE DOES NOT EXIST!"
+  echo "would you like to fetch the package from a URL?"
+  echo "[y/n]"
+  read yn
+  if [ $yn = "y" ]; then
+    echo "where is the package located? (must be tar.gz format!!)"
+    read pkglocation
+    sudo mkdir $input && sudo wget -O temp.tar.gz $pkglocation && sudo tar xf temp.tar.gz -C $input && sudo rm temp.tar.gz && cd $input
+  elif [ $yn = "n" ]; then
+    echo "'N' ARGUMENT GIVEN, EXITING."
+  else
+    echo "ERROR! TYPE 'Y' OR 'N' ONLY!" 
+  fi
+fi
+echo "does this package require the 'make install' command?"
+echo "[y/n]"
 read yn
 if [ $yn = "y" ]; then
-    sudo make && make install
+  sudo make && make install
 elif [  $yn = "n" ]; then
-    sudo make
+  sudo make
 else
-    printf "Please type 'y' or 'n':"
+  echo "ERROR! TYPE 'Y' OR 'N' ONLY!"
 fi
