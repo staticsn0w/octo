49e9382480e3d8d55d0df43f7732399cab1345bf
sad mac <staticsn0w@users.noreply.github.com>
2016-11-05 23:11:12 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-if [! -d "/usr/ports" ]; then
-  sudo mkdir /usr/ports
-fi
+printf "this is the octo installer, press [ENTER] to install, and any other key to abort the install."
+read -s -n 1 input
+if [ $input = "\x0a" ]; then
+  if [ ! -d "/usr/ports" ]; then
+    sudo mkdir /usr/ports
+  fi
 
-sudo cp octo /bin
+  sudo cp octo /bin
+  printf "Done! Run 'octo' to confirm everything is working correctly."
+else
+  printf "[ENTER] not pressed, aborting..."
+fi
