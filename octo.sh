407304529bcebce777fda4276395c35570326a52
staticsn0w <staticsn0w@hashbang.sh>
2016-11-06 00:25:38 +0000

    add subdirectory hacks and stuff

--- octo.sh
+++ octo.sh
@@ -2,10 +2,14 @@
 
 cd /usr/ports
 ls
+
+## getting package to install ##
 printf "Listed /usr/ports directory, what to install?"
 read input
 if [ -d "$input" ]; then
   cd $input
+
+## fetching package from URL if needed ##
 else
   printf "Package does not exist! Would you like to fetch the package from a URL? [y/n]"
   read yn
@@ -16,15 +20,35 @@ else
   elif [ $yn = "n" ]; then
     printf "'n' argument given, exiting.."
   else
-    printf "Please type 'y' or 'n':" 
+    printf "Please type 'y' or 'n':"
   fi
 fi
+
+## actual installation section ##
 printf "Does this package require the 'make install' command? [y/n]"
 read yn
 if [ $yn = "y" ]; then
-  sudo make && make install
+  ## Hack to fix subdirectories ##
+  if [ -f "Makefile" ]; then
+    sudo make && make install
+  elif [ ! -f "Makefile" ]; then
+    ls
+    printf "octo couldn't find a makefile, so you need to manually specify one the folders printed above that contains the makefile."
+    read mflocation
+    cd $mflocation
+    sudo make && make install
+  fi
 elif [  $yn = "n" ]; then
-  sudo make
+  ## Hack to fix subdirectories ##
+  if [ -f "Makefile" ]; then
+    sudo make
+  elif [ ! -f "Makefile" ]; then
+    ls
+    printf "octo couldn't find a makefile, so you need to manually specify one of the folders printed above that contains the makefile.\n"
+    read mflocation
+    cd $mflocation
+    sudo make
+  fi
 else
-  printf "Please type 'y' or 'n':" 
+  printf "Please type 'y' or 'n':"
 fi
